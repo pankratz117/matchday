ece90038d80875a2d5b78378e71e356cd1aa95af
Dieter Pankratz <pankratz117@gmail.com>
2020-02-06 17:09:50 +0100

    add matchdays mode validation

--- app/models/matchday.rb
+++ app/models/matchday.rb
@@ -3,4 +3,6 @@ class Matchday < ApplicationRecord
   belongs_to :sports_ground
   has_many :spots
   has_many :goals
+
+  validates :mode, inclusion: { in: ["3 on 3", "4 on 4", "5 on 5", "6 on 6", "7 on 7", "8 on 8"] }
 end
