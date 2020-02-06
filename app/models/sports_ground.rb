1f3683d416544498192f6e460bab5ff86567492f
Dieter Pankratz <pankratz117@gmail.com>
2020-02-06 10:46:18 +0100

    add SportsGround model

---
+++ app/models/sports_ground.rb
@@ -0,0 +1,2 @@
+class SportsGround < ApplicationRecord
+end
---
+++ test/models/sports_ground_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SportsGroundTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
