f5eb8d3566cb0bd6176fa517d0f35a8c1bb0f861
Dieter Pankratz <pankratz117@gmail.com>
2020-02-07 20:44:55 +0100

    should return proper view on get request #index

--- spec/controllers/matchdays_controller_spec.rb
+++ spec/controllers/matchdays_controller_spec.rb
@@ -6,5 +6,10 @@ describe MatchdaysController do
       get :index
       expect(response).to have_http_status(:ok)
     end
+
+    it 'should return proper view' do
+      get :index
+      expect(response).to render_template(:index)
+    end
   end
 end
