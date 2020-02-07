8fa2b7484ac5d613d1c1fe659f6cff712f5b275f
Dieter Pankratz <pankratz117@gmail.com>
2020-02-07 09:54:07 +0100

    clean database before creating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# 1 - cleaning database:
+
+puts 'Cleaning database...'
+
+def delete(db_model)
+  puts "deleting #{db_model.to_s.downcase}s..."
+  db_model.destroy_all
+end
+
+db_models = [Goal, Spot, Matchday, User, SportsGround]
+
+db_models.each do |db_model|
+  unless db_model.all.empty?
+    delete(db_model)
+  end
+end
+
+
+
+
