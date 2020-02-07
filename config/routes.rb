92987f82f581bf053d85c5ac980e2d52c0d149e1
Dieter Pankratz <pankratz117@gmail.com>
2020-02-07 11:43:45 +0100

    add routes to list all matchdays

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'pages/home'
-  get 'pages/contact'
-  get 'pages/about'
+  get 'matchdays', to: 'matchdays#index', as: :matchdays
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
