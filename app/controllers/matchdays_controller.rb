4e5ad64650d3e57fef792779bc36497826294a2d
Dieter Pankratz <pankratz117@gmail.com>
2020-02-14 12:32:10 +0100

    add matchdays#new action

--- app/controllers/matchdays_controller.rb
+++ app/controllers/matchdays_controller.rb
@@ -7,4 +7,8 @@ class MatchdaysController < ApplicationController
     @matchday = Matchday.find(params[:id])
     @spots = @matchday.spots
   end
+
+  def new
+    @matchday = Matchday.new
+  end
 end
