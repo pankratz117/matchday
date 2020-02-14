2ce46369d28f6c09a0abd26126d640eeaea33d56
Dieter Pankratz <pankratz117@gmail.com>
2020-02-14 12:37:54 +0100

    add matchdays#new and matchdays#create actions

--- app/controllers/matchdays_controller.rb
+++ app/controllers/matchdays_controller.rb
@@ -11,4 +11,17 @@ class MatchdaysController < ApplicationController
   def new
     @matchday = Matchday.new
   end
+
+  def create
+    @matchday = Matchday.new(matchday_params)
+    @matchday.save
+
+    redirect_to matchday_path(@matchday)
+  end
+
+  private
+
+  def matchday_params
+    params.require(:matchday).permit(:user, :sport_type, :date, :red_score, :blue_score, :mode, :sports_ground)
+  end
 end
