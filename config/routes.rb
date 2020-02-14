6cae2ea9c883438ad7efdadbf5d1c0d663a199c2
Dieter Pankratz <pankratz117@gmail.com>
2020-02-14 12:39:15 +0100

    add routes for #new and #create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :matchdays, only: [:index, :show]
+  resources :matchdays, only: [:index, :show, :new, :create]
 end
