758f962d9f5cf08058c053c8ccb56494d7638232
Dieter Pankratz <pankratz117@gmail.com>
2020-02-15 12:46:35 +0100

    fixed the form to create a matchday

--- app/controllers/matchdays_controller.rb
+++ app/controllers/matchdays_controller.rb
@@ -15,9 +15,8 @@ class MatchdaysController < ApplicationController
 
   def create
     @matchday = Matchday.new(matchday_params)
-    sports_ground_id = params[:sports_ground_id].to_i
-    sports_ground = SportsGround.find(sports_ground_id)
-    @matchday.sports_ground = sports_ground
+    @sports_ground = SportsGround.find(matchday_params[:sports_ground_id].to_i)
+    @matchday.sports_ground = @sports_ground
     @matchday.user = current_user
     @matchday.save!
 
@@ -27,7 +26,6 @@ class MatchdaysController < ApplicationController
   private
 
   def matchday_params
-    # params.require(:matchday).permit(:authenticity_token, :user, :sport_type, :date, :red_score, :blue_score, :mode, :sports_ground, :sports_ground_id)
-    params.require(:matchday).permit!
+    params.require(:matchday).permit(:authenticity_token, :user, :sport_type, :date, :red_score, :blue_score, :mode, :sports_ground, :sports_ground_id)
   end
 end
